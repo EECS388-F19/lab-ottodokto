8ea149df51af08874be0843a537c68808a32752c
Brian Lou <louyi@umich.edu>
2019-09-09 14:43:23 -0400

    added helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+from random import randint
+
+print("Brian Lou")
+a = randint(1,100)
+b = randint(1,100)
+
+result = a + b
+avg = average(a,b)
+
+print(a)
+print(b)
+print("Sum = " + str(result)) 
+print("Average = " + str(avg))
